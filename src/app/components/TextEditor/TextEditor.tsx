2204b83b1d3bfbd69cb40ede71c25e9741dfc39f
Madhav Gautam <madhavgautam2016@gmail.com>
2023-02-11 11:20:25 +0545

    updates on seprate rooms handling from url query and content sharing

--- src/app/components/TextEditor/TextEditor.tsx
+++ src/app/components/TextEditor/TextEditor.tsx
@@ -2,38 +2,34 @@
 import React, { useState, useEffect } from 'react';
 import { Textarea, Grid, useInput, Spacer, Button } from '@nextui-org/react';
 import io from 'socket.io-client';
+import { useSearchParams } from 'next/navigation';
 
 const socket = io('http://localhost:3000');
 
 export default function TextEditor() {
-	const [message, setMessage] = useState('');
-	const [messages, setMessages] = useState([]);
-	let roomName = 'testRoomName';
+	const searchParams = useSearchParams();
+	const room = searchParams.get('room');
 
-	console.log('received herer');
-	socket.on('connect', () => {
-		console.log('Connected');
+	const [roomName, setRoomName] = useState(
+		room || Math.random().toString(32)
+	);
+	useEffect(() => {
 		socket.emit('joinRoom', roomName);
-	});
 
-	socket.on('newuser', (data) => {
-		console.log(data);
-	});
-
-	socket.on('newcontent', (data) => {
-		console.log(data);
-	});
-
-	socket.on('disconnect', function () {
-		console.log('Disconnected');
-	});
-
-	const handleSubmit = (event: any) => {
-		socket.emit('shareContent', {
-			text: 'This is the textcontent',
-			roomName: roomName
+		socket.on('newcontent', (data) => {
+			textareaRef.current.value = data.text;
 		});
-	};
+
+		socket.on('disconnect', function () {
+			console.log('Disconnected');
+		});
+
+		return () => {
+			socket.off('connect');
+			socket.off('newcontent');
+			socket.off('disconnect');
+		};
+	}, []);
 
 	// Controlled
 	const {
@@ -41,21 +37,30 @@ export default function TextEditor() {
 		setValue: setControlledValue,
 		reset,
 		bindings
-	} = useInput('Input Text and Share with all....');
+	} = useInput('Enter your content here...');
 
 	// Uncontrolled
 	const textareaRef = React.useRef(null);
-	const onPress = () => {
+
+	const handleSubmit = () => {
+		let content = '';
 		if (textareaRef.current) {
-			textareaRef.current.value = Math.random().toString(32);
+			content = textareaRef.current.value;
 		}
+
+		socket.emit('shareContent', {
+			text: content,
+			roomName: roomName
+		});
 	};
+
 	return (
 		<div style={{ margin: '10px' }}>
 			<Grid.Container gap={1} justify={'center'}>
 				<Grid xs={12}>
 					<Textarea
 						{...bindings}
+						ref={textareaRef}
 						fullWidth
 						minRows={100}
 						maxRows={30}
@@ -67,9 +72,9 @@ export default function TextEditor() {
 						auto
 						color="primary"
 						size="sm"
-						onPress={() => handleSubmit(Math.random().toString(32))}
+						onPress={() => handleSubmit()}
 					>
-						Share Content
+						BroadCast Message
 					</Button>
 				</Grid>
 			</Grid.Container>
